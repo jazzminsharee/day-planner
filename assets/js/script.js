fc8b5b7e6a92be7fa2d6b8a6754212aa77464760
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-11-06 10:29:26 -0700

    setup layout for time blocks in html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// layout of rows from 9am-5pm
+// input text to change events and lick on the side to save it to local storage
+// moment.js says what day it is at the top of the screen and keeps the current time
+// events in the planner change color based on past hours, current hour, and future hours
+
+// var = 
+
+// current day is displayed at the top of the calendar
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
